4dfb60b92e0f1931b6aa908a4e9326af73cb38c6
Philipp Schmitt <philipp@schmitt.co>
2025-02-04 14:55:19 +0100

    lets try something else

--- z-a-python.plugin.zsh
+++ z-a-python.plugin.zsh
@@ -6,23 +6,23 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload :za-py-atclone-handler :za-py-atpull-handler :za-py-atdelete-handler
+autoload za-py-atclone-handler za-py-atpull-handler za-py-atdelete-handler
 
 # An empty stub to fill the help handler fields
 :za-py-null-handler() { :; }
 
 @zinit-register-annex "z-a-python" \
   hook:atclone-50 \
-  :za-py-atclone-handler \
-  :za-py-null-handler \
+  za-py-atclone-handler \
+  za-py-null-handler \
   "uv|uv''" # register new ices
 
 @zinit-register-annex "z-a-python" \
   hook:\%atpull-50 \
-  :za-py-atpull-handler \
-  :za-py-null-handler
+  za-py-atpull-handler \
+  za-py-null-handler
 
 @zinit-register-annex "z-a-python" \
   hook:atdelete-50 \
-  :za-py-atdelete-handler \
-  :za-py-null-handler
+  za-py-atdelete-handler \
+  za-py-null-handler
