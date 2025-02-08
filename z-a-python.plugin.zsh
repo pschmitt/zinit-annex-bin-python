cd6c0c8f699184f5b9cc04dbe73fa6d42f4aee70
Philipp Schmitt <philipp@schmitt.co>
2025-02-08 08:41:57 +0100

    Remove {py,}pip ice as it clashes with node-bin-gem annex as well as our own pipx ice

--- z-a-python.plugin.zsh
+++ z-a-python.plugin.zsh
@@ -15,7 +15,7 @@ autoload za-py-handler
   hook:\!atclone-50 \
   za-py-handler \
   za-py-null-handler \
-  "uv|uv''|pypip|pypip''|pipx|pipx''" # register new ices
+  "uv|uv''|pypipx|pypipx''" # register new ices
 
 @zinit-register-annex "z-a-python" \
   hook:\%atpull-50 \
